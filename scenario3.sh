cde118af0443fd2689248ff0cd0f9636f136bf51
Patrick <44551053+Forcebyte@users.noreply.github.com>
2019-03-04 22:52:33 -0500

    Update scenario3.sh

--- scenario3.sh
+++ scenario3.sh
@@ -1,112 +1,118 @@
 #!/bin/bash
 		
 #Flush Tables, Set Forward to Drop and everything else to accept
-
 iptables -F
 iptables -X
-
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
-# Create logging chain for accept packets on INPUT
-iptables -N accept-input
-iptables -A accept-input -j LOG --log-prefix "INPUT-ACCEPTED "
-iptables -A accept-input -j ACCEPT
 
-# Create logging chain for dropped packets on INPUT
-iptables -N drop-input
-iptables -A drop-input -j LOG --log-prefix "INPUT-DROPPED "
-iptables -A drop-input -j DROP
+####
+# INPUT CHAIN LOGGING
+	# Create logging chain for accept packets on INPUT
+	iptables -N accept-input
+	iptables -A accept-input -j LOG --log-prefix "INPUT-ACCEPTED "
+	iptables -A accept-input -j ACCEPT
 
+	# Create logging chain for dropped packets on INPUT
+	iptables -N drop-input
+	iptables -A drop-input -j LOG --log-prefix "INPUT-DROPPED "
+	iptables -A drop-input -j DROP
+####
 
-# Create logging chain for accepted packets on OUTPUT
-iptables -N accept-output
-iptables -A accept-output -j LOG --log-prefix "OUTPUT-ACCEPTED "
-iptables -A accept-output -j ACCEPT
+####
+# OUTPUT CHAIN LOGGING
+	# Create logging chain for accepted packets on OUTPUT
+	iptables -N accept-output
+	iptables -A accept-output -j LOG --log-prefix "OUTPUT-ACCEPTED "
+	iptables -A accept-output -j ACCEPT
 
-# Create logging chain for drop packets on OUTPUT
-iptables -N drop-output
-iptables -A drop-output -j LOG --log-prefix "OUTPUT-DROPPED " 
-iptables -A drop-output -j DROP
+	# Create logging chain for drop packets on OUTPUT
+	iptables -N drop-output
+	iptables -A drop-output -j LOG --log-prefix "OUTPUT-DROPPED " 
+	iptables -A drop-output -j DROP
+####
 
+####
+# FORWARD CHAIN LOGGING
+	# Create logging chain for accepted packets on FORWARD
+	iptables -N accept-forward
+	iptables -A accept-forward -j LOG --log-prefix "FORWARD-ACCEPTED "
+	iptables -A accept-forward -j ACCEPT
 
-# Create logging chain for accepted packets on FORWARD
-iptables -N accept-forward
-iptables -A accept-forward -j LOG --log-prefix "FORWARD-ACCEPTED "
-iptables -A accept-forward -j ACCEPT
-
-# Create logging chain for dropped packets on FORWARD
-iptables -N drop-forward
-iptables -A drop-forward -j LOG --log-prefix "FORWARD-DROPPED "
-iptables -A drop-forward -j DROP
+	# Create logging chain for dropped packets on FORWARD
+	iptables -N drop-forward
+	iptables -A drop-forward -j LOG --log-prefix "FORWARD-DROPPED "
+	iptables -A drop-forward -j DROP
 # -------
 
 #- Scenerio 3 -#  
 #Configures your firewall to connect to everything BUT
-#SSH To the router
+	# SSH To the router
 
+# DHCP
+	# Allow DHCP Forwarding only to WinServ and the Client (Specified via interface)
+	iptables -A INPUT -p udp --dport 67:68 --sport 67:68 -j accept-forward
+	iptables -A OUTPUT -p udp --dport 67:68 --sport 67:68 -j accept-forward 
+	iptables -A FORWARD -p udp --dport 67:68 --sport 67:68 -j accept-forward
 
-# Allow DHCP Forwarding only to WinServ and the Client (Specified via interface)
-iptables -A INPUT -p udp --dport 67:68 --sport 67:68 -j accept-forward
-iptables -A OUTPUT -p udp --dport 67:68 --sport 67:68 -j accept-forward 
-iptables -A FORWARD -p udp --dport 67:68 --sport 67:68 -j accept-forward
+# SSH
+	# Allow SSH/SCP Traffic to and from Router
+	   # DONT ALLOW SSH SSH TRAFFIC
+		iptables -A INPUT -p tcp -s 195.165.52.0/26 --dport 3737 -m state --state NEW,ESTABLISHED,RELATED -j drop-input
+		iptables -A OUTPUT -p tcp -d 195.165.52.0/26 --sport 3737 -m state --state NEW,RELATED,ESTABLISHED -j drop-output
+	   # SCP Traffic
+		iptables -A INPUT -p tcp -s 195.165.52.0/26  --dport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-input
+		iptables -A OUTPUT -p tcp -d 195.165.52.0/26  --sport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-output
 
+	# Allow SSH/SCP Traffic anywhere else
+	   # DONT ALLOW SSH TRAFFIC
+		iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 3737 -m state --state NEW,RELATED,ESTABLISHED -j drop-forward
+		iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 3737 -m state --state NEW,RELATED,ESTABLISHED -j drop-forward
+	   # SCP Traffic
+		iptables -A FORWARD -p tcp -s 195.165.52.0/26  --dport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+		iptables -A FORWARD -p tcp -d 195.165.52.0/26  --sport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
-# Allow SSH/SCP Traffic to and from Router
-#SSH Traffic
-iptables -A INPUT -p tcp -s 195.165.52.0/26 --dport 3737 -m state --state NEW,ESTABLISHED,RELATED -j accept-input
-iptables -A OUTPUT -p tcp -d 195.165.52.0/26 --sport 3737 -m state --state NEW,RELATED,ESTABLISHED -j accept-output
-#SCP Traffic
-iptables -A INPUT -p tcp -s 195.165.52.0/26  --dport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-input
-iptables -A OUTPUT -p tcp -d 195.165.52.0/26  --sport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-output
-
-# Allow SSH/SCP Traffic anywhere else
-#SSH Traffic
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 3737 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 3737 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
-#SCP Traffic
-iptables -A FORWARD -p tcp -s 195.165.52.0/26  --dport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26  --sport 7373 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
 # Allow hMail to have access to IMAP and SMTP traffic
-#IMAP
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 143 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 143 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-#SMTP
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 25 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 25 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-#SMTP
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 587 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 587 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+   # IMAP
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 143 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 143 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+   # SMTP
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 25 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 25 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+   # SMTP
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 587 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 587 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
 # Allow FTP (Filezilla, Unencrypted) Traffic
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 20:21 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 20:21 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 20:21 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 20:21 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
 # Allow MySQL Traffic
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 3306 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 3306 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 3306 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 3306 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
 # Allow Tracert forwarding 
-iptables -A FORWARD -p udp -s 195.165.52.0/26 --dport 33434:33534 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
-iptables -A FORWARD -p udp -d 195.165.52.0/26 --sport 33434:33534 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p udp -s 195.165.52.0/26 --dport 33434:33534 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
+	iptables -A FORWARD -p udp -d 195.165.52.0/26 --sport 33434:33534 -m state --state NEW,ESTABLISHED,RELATED -j accept-forward
 
 # Allow Apache forwarding (custom port)
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 4242 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 4242 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 4242 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 4242 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
 
 # Allow IIS forwarding (custom port)
-iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 2424 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
-iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 2424 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
+	iptables -A FORWARD -p tcp -s 195.165.52.0/26 --dport 2424 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
+	iptables -A FORWARD -p tcp -d 195.165.52.0/26 --sport 2424 -m state --state NEW,RELATED,ESTABLISHED -j accept-forward
 
 # Allow Incoming/Outgoing DNS forwarding requests
-#UDP
-iptables -A FORWARD -p udp --dport 53 -j accept-forward
-iptables -A FORWARD -p udp --sport 53 -j accept-forward
-#TCP
-iptables -A FORWARD -p tcp --dport 53 -j accept-forward
-iptables -A FORWARD -p tcp --sport 53 -j accept-forward
+	   # UDP
+		iptables -A FORWARD -p udp --dport 53 -j accept-forward
+		iptables -A FORWARD -p udp --sport 53 -j accept-forward
+	   # TCP
+		iptables -A FORWARD -p tcp --dport 53 -j accept-forward
+		iptables -A FORWARD -p tcp --sport 53 -j accept-forward
 
 
 # Allow ICMP Forwarding (Testing purposes Only)
